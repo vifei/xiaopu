b2d4416b0907b3677e8917461eb2cf9f5f9d69d4
zhifei <sshy6464@163.com>
2023-12-24 20:32:06 +0800

    登录实现

--- app/src/main/java/com/example/xiaopu/Login.java
+++ app/src/main/java/com/example/xiaopu/Login.java
@@ -3,12 +3,93 @@ package com.example.xiaopu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.xiaopu.utils.HttpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
+
+    //控件
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_login;
+
+    //全局变量
+    private boolean password_currect = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+        initEvent();
     }
+
+    public void initView(){
+        et_username = this.findViewById(R.id.ed_login_account);
+        et_password = this.findViewById(R.id.edt_login_password);
+        btn_login = this.findViewById(R.id.btn_login);
+    }
+
+    public void initEvent(){
+        //给登录按钮添加点击事件(登录)
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取用户名和密码
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+
+                JSONObject jsonData  = new JSONObject();
+                try {
+                    jsonData.put("number", username);
+                    jsonData.put("password", password);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                String jsonString = jsonData.toString();
+
+                //调用API验证用户名密码是否正确
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = HttpUtils.postJsonContent("http://849p815u54.zicp.fun:48340/user/login", jsonString);
+                        try {
+                            JSONObject jsonObject = new JSONObject(result);
+
+                            if(jsonObject.getInt("code") == 200) {
+                                password_currect = true;
+                            } else {
+                                password_currect = false;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //登录事件
+                if(password_currect) {
+                    Toast.makeText(Login.this, "登录成功！", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Login.this, "密码错误！", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/xiaopu/utils/HttpUtils.java
@@ -0,0 +1,73 @@
+package com.example.xiaopu.utils;
+
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpUtils {
+
+    public HttpUtils(){
+
+    }
+
+    public static String postJsonContent(String url_path, String jsonData) {
+        try{
+            URL url = new URL(url_path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(3000);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            OutputStream outputStream = connection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            // 将JSON数据写入输出流
+            bufferedWriter.write(jsonData);
+            // 刷新缓冲区
+            bufferedWriter.flush();
+            // 关闭写入流
+            bufferedWriter.close();
+            // 关闭输出流
+            outputStream.close();
+
+
+            int code = connection.getResponseCode();
+
+            if(code == 200){
+                return changeInputStream(connection.getInputStream());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String changeInputStream(InputStream inputStream) {
+        String jsonString = "";
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int len = 0;
+        byte[] data = new byte[1024];
+        try{
+            while ((len = inputStream.read(data)) != -1) {
+                outputStream.write(data, 0, len);
+            }
+            jsonString = new String(outputStream.toByteArray());
+            return jsonString;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
